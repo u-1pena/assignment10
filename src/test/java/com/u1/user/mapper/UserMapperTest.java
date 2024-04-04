888054caccd317d4a87338820331ed5de5367f63
u-1 <shimaichi5973@gamil.com>
2024-04-04 22:13:17 +0900

    READ処理の単体テストとCIの実装

--- src/test/java/com/u1/user/mapper/UserMapperTest.java
+++ src/test/java/com/u1/user/mapper/UserMapperTest.java
@@ -9,6 +9,7 @@ import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -24,6 +25,7 @@ class UserMapperTest {
             scripts = {"classpath:/sqlannotation/delete-users.sql", "classpath:/sqlannotation/insert-users.sql"},
             executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD
     )
+
     @Transactional
     void 全てのユーザーが取得できること() {
         List<User> users = userMapper.findAll();
@@ -34,5 +36,21 @@ class UserMapperTest {
                         new User(2, "kana", "1993-12-31"),
                         new User(3, "tom", "1995-09-25")
                 );
+    }
+
+    @Test
+
+    @Transactional
+    void 存在するユーザーのIDを指定した時に正常にユーザーが返されること() {
+        Optional<User> users = userMapper.findById(1);
+        assertThat(users).contains(new User(1, "yuichi", "1984-07-03"));
+    }
+
+    @Test
+
+    @Transactional
+    void 存在しないユーザーIDを指定した時に例外処理が返されること() {
+        Optional<User> users = userMapper.findById(500);
+        assertThat(users).isEmpty();
     }
 }
--- src/test/java/com/u1/user/service/UserServiceTest.java
+++ src/test/java/com/u1/user/service/UserServiceTest.java
@@ -1,5 +1,6 @@
 package com.u1.user.service;
 
+import com.u1.user.controller.response.UserNotFoundException;
 import com.u1.user.entity.User;
 import com.u1.user.mapper.UserMapper;
 import org.junit.jupiter.api.Test;
@@ -9,8 +10,10 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.verify;
 
@@ -36,4 +39,22 @@ public class UserServiceTest {
         verify(userMapper).findAll();
     }
 
+    @Test
+    void 存在するユーザーのIDを指定した時に正常にユーザーが返されること() throws Exception {
+        doReturn(Optional.of(new User(1, "yuichi", "1984-07-03"))).when(userMapper).findById(1);
+
+        User actual = userService.findUser(1);
+        assertThat(actual).isEqualTo(new User(1, "yuichi", "1984-07-03"));
+        verify(userMapper).findById(1);
+    }
+
+    @Test
+    void 存在しないユーザーIDを指定した時に例外処理が返されること() throws UserNotFoundException {
+        doReturn(Optional.empty()).when(userMapper).findById(0);
+        assertThrows(UserNotFoundException.class, () -> {
+            userService.findUser(0);
+        });
+        verify(userMapper).findById(0);
+
+    }
 }
